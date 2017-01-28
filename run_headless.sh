1df5ce0b0ca32a27f61e589dcd8e78254a472c4a
wasd <wasdsweden@gmail.com>
2017-01-28 22:28:24 +0100

    Update run headless script

--- run_headless.sh
+++ run_headless.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-../teeworlds HEADLESS "player_name WASD-bot" "connect 127.0.0.1:8303"
+ip="127.0.0.1:8303"
+if [[ $# -ne 0 ]]; then
+  ip=$1
+fi
+../teeworlds HEADLESS "player_name WASD-bot" "connect $ip"
